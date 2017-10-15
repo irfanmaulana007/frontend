0d75a0e57c6c40d1f0102334e29e465d22bc2208
irfan maulana <irfanmaulana007@icloud.com>
2017-10-16 00:23:48 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+        
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/styles.css') }}">
+        <script src="{{ asset('js/app.js') }}"></script>
+
+        <link rel="stylesheet" type="text/css" href="{{ asset('bower_components/animate.css/animate.min.css') }}">
+
+    </head>
+    <body>
+        <div class="container">
+            <br><br>
+            <div class="fadeIn animated">
+                <h1 class=""><b>Irfan Maulana</b></h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <br>
+
+            <div class="row">
+                <div class="col-sm-3">
+                    <div class="card card1 hover fadeInUp animated">
+                        <div class="text-overlay">
+                            <h3>Detail..</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="card card2 hover fadeInUp animated">
+                        <div class="text-overlay">
+                            <h3>Click me!!</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="card card3 hover fadeInUp animated">
+                        <div class="text-overlay">
+                            <h3>More..</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="card card4 hover fadeInUp animated">
+                        <div class="text-overlay">
+                            <h3>Well</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <footer>
+                <h5 class="text-center">Designed and Developed by <a href="https://www.instagram.com/irfanmaulana007/" target="_blank">Irfan</a></h5>
+            </footer>
+        </div>
+
+        <script>
+
+        </script>
+    </body>
+</html>
