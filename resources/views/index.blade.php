b1771b122d4d7e05e482a83bda0fc3cf10c827df
irfan maulana <irfanmaulana007@icloud.com>
2017-10-17 00:10:19 +0700

    adding vue js!!

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,19 +12,35 @@
         <script src="{{ asset('js/app.js') }}"></script>
 
         <link rel="stylesheet" type="text/css" href="{{ asset('bower_components/animate.css/animate.min.css') }}">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
 
     </head>
     <body>
         <div class="container">
             <br><br>
             <div class="fadeIn animated">
-                <h1 class=""><b>Irfan Maulana</b></h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div class="row">
+                    <h1 class="pull-left"><b>Irfan Maulana</b></h1>
+                    <h2 id="date" class="pull-right"></h2>
+                </div>
+                <div class="row">
+                    <div id="welcome">
+                     @{{ message }}
+                    </div>
+                    <div id="on-learning">
+                      <ol>
+                        <li v-for="todo in todos">
+                          @{{ todo.text }}
+                        </li>
+                      </ol>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
             </div>
             <br>
 
@@ -51,11 +67,7 @@
                     </div>
                 </div>
                 <div class="col-sm-3">
-                    <div class="card card4 hover fadeInUp animated">
-                        <div class="text-overlay">
-                            <h3>Well</h3>
-                        </div>
-                    </div>
+                    <todo-card></todo-card>
                 </div>
             </div>
 
@@ -65,7 +77,37 @@
         </div>
 
         <script>
+            $(document).ready(function(){
+                date = moment().format('dddd, Do MMMM YYYY');
+                moment().utcOffset(7);
+                $("#date").html(date);
+            });
 
+            var app = new Vue({
+              el: '#welcome',
+              data: {
+                message: 'Now i learn something new on frontend development, such as:'
+              }
+            })
+
+            var learn = new Vue({
+              el: '#on-learning',
+              data: {
+                todos: [
+                  { text: 'Frontend' },
+                  { text: 'Vue' },
+                  { text: 'Bower' },
+                  { text: 'Something cool' },
+                  { text: 'Git' },
+                  { text: 'Laravel' }
+                ]
+              }
+            })
+
+            Vue.component('todo-card', {
+                props: ['todo'],
+                template: '<div class="card card4 hover fadeInUp animated"><div class="text-overlay"><h3>Well</h3></div></div>'
+            })
         </script>
     </body>
 </html>
